7c2c8506ea57a4be524d8a79c9f48cb7a8826767
Adlu Bagus Irawan <adlubagusi@gmail.com>
2019-04-15 22:22:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$connect = mysql_connect("localhost","root","310801") or die("Gagal Koneksi");
+	$connect = mysql_connect("localhost","root","") or die("Gagal Koneksi");
 	if(mysql_select_db("db_pinjam_barang")){
 		//echo "DATABASE TERPILIH: db_pinjam_barang";
 	}
